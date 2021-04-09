cb61c8690ea99288f5d61964cae7659aaae04dba
smendescoti <sergio.coti@gmail.com>
2021-04-08 22:10:46 -0300

    Modificações nos serviços da API e Repositório

--- src/main/java/br/com/cotiinformatica/interfaces/IContaRepository.java
+++ src/main/java/br/com/cotiinformatica/interfaces/IContaRepository.java
@@ -11,7 +11,8 @@ import br.com.cotiinformatica.entities.Conta;
 
 public interface IContaRepository extends CrudRepository<Conta, Integer> {
 
-	@Query("select c from Conta c where c.data >= :param1 and c.data <= :param2")
-	List<Conta> findAllByDatas(@Param("param1") Date dataMin, @Param("param2") Date dataMax);
+	@Query("select c from Conta c where c.data >= :param1 and c.data <= :param2 and c.usuario.idUsuario = :param3")
+	List<Conta> findAllByDatas(@Param("param1") Date dataMin, @Param("param2") Date dataMax,
+			@Param("param3") Integer idUsuario);
 
 }
